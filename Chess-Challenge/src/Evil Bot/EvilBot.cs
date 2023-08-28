d0348d675826ed208d1316a441d124415e7a6ebc
Daniel Wolff <daniel.ryan.wolff@gmail.com>
2023-08-27 17:58:05 -0700

    setting EvilBot to WolfuhBot

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -3,152 +3,25 @@ using System;
 
 public class EvilBot : IChessBot
 {
-    ulong[,] mg_pesto_values = {
-        {
-            9187201950435737471,
-            7889872735987012212,
-            8249888057508007801,
-            8178110886297371762,
-            8684490983700006771,
-            8971887951866465141,
-            12736916069520674937,
-            9187201950435737471,
-        },
-        {
-            5365021282678175091,
-            8098731609819740277,
-            8321110065793174391,
-            8683370564221962619,
-            8829175787637803146,
-            7466283872183886741,
-            6515199656436073078,
-            3121678878726059849,
-        },
-        {
-            7961652157893667700,
-            9333296259492581247,
-            9189180002037172356,
-            8972724671372166273,
-            9043659715595305598,
-            8615830923643490686,
-            8252695102340499559,
-            8106855828019643003,
-        },
-        {
-            8464655719713631346,
-            7599672065180073051,
-            7526209278135204974,
-            7886499481757909619,
-            8320825326692236149,
-            8973576814208589191,
-            10128486292210617493,
-            10346052129634094740,
-        },
-        {
-            9112605605389037414,
-            7889044859766865279,
-            8683073665731954305,
-            8823249286498975869,
-            8174446153761783423,
-            8680268833331320475,
-            8316878023779126682,
-            8178411053325325461,
-        },
-        {
-            8615814228884949894,
-            9188041839510782851,
-            8680816273551030129,
-            7385465145061371493,
-            8535862196875458669,
-            8830292744950942324,
-            10195715779626626160,
-            6812406333190340741,
-        },
-    };
-    
-    ulong[,] eg_pesto_values = {
-        {
-            9187201950435737471,
-            9620677834192158843,
-            9332158264906645115,
-            9620671198299652222,
-            10343507760406169479,
-            12588028940747647145,
-            15624621818218926556,
-            9187201950435737471,
-        },
-        {
-            8099006461787924063,
-            7671172215411667817,
-            8321946802664076660,
-            8537848026931626358,
-            8538977221128520566,
-            8319701586934396266,
-            8249313030004962149,
-            7092175941798027085,
-        },
-        {
-            8321090239988202614,
-            8680261115071592305,
-            8754297855876823927,
-            8971317279073795450,
-            9044219276634128512,
-            9258133640255274881,
-            8898411897061670264,
-            8679695953109350003,
-        },
-        {
-            8827196550388679029,
-            8970184747602573693,
-            9043083459462527863,
-            9259826901013003129,
-            9331605210574847616,
-            9404222464637893757,
-            9549185388996756352,
-            9620964815317336961,
-        },
-        {
-            7958270012948444522,
-            8391174189128838511,
-            8606808257992295553,
-            8542634248410010250,
-            9262369058317376401,
-            8467474936499308675,
-            8541516032434212479,
-            8830022355671090313,
-        },
-        {
-            7236849716174549865,
-            8176709008955112566,
-            8466068600795923066,
-            8538131701015675769,
-            8902081049221106816,
-            9549753845300958597,
-            8757115917098453636,
-            6515994487092052342,
-        },
-    };
 
-    ulong[] mg_piece_value = { 0, 82, 337, 365, 477, 1025, 10000 };
-    ulong[] eg_piece_value = { 0, 94, 281, 297, 512, 936, 10000 };
-    int[] phase_incs = { 0, 0, 1, 1, 2, 4, 0 };
+    /* TODO
+        - Reverse engineer eval function.
+        - (WolfuhBot) Implement getPstVal into order non-cap moves.
+        - (WolfuhBot) Bring in killer moves and null windows
+    */
 
     Move choice;
-    int timeout;
 
-#if DEBUG
-    int nodes, choiceScore;
-#endif
     Board board;
     Timer timer;
 
     (Move move, ulong key, int depth, int flag, int score)[] tt = {};
     Move[,] killers;
 
-    int MAX = 10000000;
+    int MAX = 10000000, timeout;
 
     public EvilBot() {
-        Array.Resize(ref tt, 0x7FFFFF + 1);
+        Array.Resize(ref tt, 0x7FFFFF);
     }
 
     public Move Think(Board board, Timer timer)
@@ -157,180 +30,177 @@ public class EvilBot : IChessBot
         this.timer = timer;
         killers = new Move[60,2];
 
-        choice = Move.NullMove;
+        // Default to some move, determine our minimum search time.
+        choice = board.GetLegalMoves()[0];
         timeout = timer.MillisecondsRemaining / 30;
 
-#if DEBUG
-        choiceScore = -MAX;
-#endif
+        // 5ms or less, panic and hope the other bot times out first.
+        if (timeout < 5) return choice;
 
+        // Iterative deepening with time constraint.
         int depth = 1;
         while (timer.MillisecondsElapsedThisTurn < timeout && depth <= 30) {
-
-#if DEBUG
-            nodes = 0;
-#endif
-
-            int score = Search(depth++, 0, -MAX, MAX);
-            if (score > MAX/2) break;
-
-#if DEBUG
-            /*
-            Debug(0, 
-                "MBOT depth=" + (depth-1) +
-                "; nodes=" + nodes +
-                "; score=" + choiceScore +
-                "; time=" + timer.MillisecondsElapsedThisTurn +
-                "; " + choice
-            );
-            */
-#endif
+            Search(depth++, 0, -MAX, MAX);
         }
 
-#if DEBUG
-        //Debug(0, "MBOT committing " + choice + " with a score of " + choiceScore);
-#endif
-
-        // Debug this issue
-        if (choice.IsNull) choice = board.GetLegalMoves()[0];
+        // Return our best result.
         return choice;
     }
 
     int Search(int depth, int ply, int alpha, int beta) {
-#if DEBUG
-        nodes++;
-#endif
 
-        bool queisce = depth <= 0, isRoot = ply == 0;
+        bool queisce = depth <= 0, notRoot = ply > 0;
         ulong key = board.ZobristKey;
         int oAlpha, result = -MAX;
 
-        if (!isRoot && board.IsRepeatedPosition()) return 0;
+        // Handle repeated move draw scenario.
+        if (notRoot && board.IsRepeatedPosition()) return 0;
 
+        // Take a look in the transposition table, see if we can return early.
         var ttEntry = tt[key % 0x7FFFFF];
-        if (!isRoot && ttEntry.key == key && ttEntry.depth >= depth && (
+        if (notRoot && ttEntry.key == key && ttEntry.depth >= depth && (
                 ttEntry.flag == 0 
                 || (ttEntry.flag == 1 && ttEntry.score <= alpha) 
                 || (ttEntry.flag == 2 && ttEntry.score >= beta)
             )) return ttEntry.score;
 
+        // Otherwise take the transposition entry as our best move, even if it
+        // might not exist.
         Move bestMove = ttEntry.move;
 
+        // Handle Queiscent inside of our search function to save tokens.
         if (queisce) {
-            result = Score();
+            result = Evaluate();
             if (result >= beta) return result;
             alpha = Math.Max(alpha, result);
         }
 
+        // Get moves, captures only if we're in queiscent search and we're not in check.
         Move[] moves = board.GetLegalMoves(queisce && !board.IsInCheck());
-        if (!queisce && moves.Length == 0) return board.IsInCheck() ? -MAX + ply+1 : 0;
 
+        // If we're in regular search and there are no moves, it's a draw if there is no
+        // check and we've lost if there is a check.
+        if (!queisce && moves.Length == 0) return board.IsInCheck() ? ply - MAX : 0;
+
+        // Generate our move ordering weights for our current move selection.
         int[] ordering = Array.ConvertAll(moves, move =>
+            // If our transposition table move is found, weight it the highest.
             (move == bestMove ? MAX : 0) +
+            // If the move is a capture, weight it by the captured piece and then by the
+            // capturing piece.
             (move.IsCapture 
                 ? 1000 * (int)move.CapturePieceType - (int)move.MovePieceType 
-                : (killers[ply, 0] == move || killers[ply, 1] == move ? 900 : 0))
+                : killers[ply, 0] == move || killers[ply, 1] == move 
+                    ? 900 
+                    : 0
+            )
         );
 
+        // Save our original alpha for after we process our moves.
         oAlpha = alpha;
 
-        for (int index = 0, pick; index < moves.Length; index++) {
+        for (int index = 0; index < moves.Length; index++) {
+
+            // If we've reached the time limit, stop now.
             if (timer.MillisecondsElapsedThisTurn > timeout) {
-                return MAX;
+                return MAX; // MAX ensures this path won't be considered.
             }
 
-            pick = index;
-            for (int j = index; j < moves.Length; j++) {
+            // Pick the next best move to evaluate here.
+            int pick = index, j = index;
+            while (++j < moves.Length) {
                 if (ordering[j] > ordering[pick]) pick = j;
             }
             (ordering[index], ordering[pick], moves[index], moves[pick]) = (ordering[pick], ordering[index], moves[pick], moves[index]);
             Move move = moves[index];
 
+            // Make the move, search it at the next ply.
             board.MakeMove(move);
-            int score;
-            if (index == 0) {
-                score = -Search(depth - 1, ply + 1, -beta, -alpha);
-            }
-            else {
-                score = -Search(depth - 1, ply + 1, -alpha - 1, -alpha);
+            
+            bool isPvNode = index == 0;
+            int reduction = queisce ? 1 : Math.Min(
+                depth, 
+                (isPvNode ? 1 : 2) - (board.IsInCheck() ? 1 : 0)
+            );
+
+            int score = -Search(depth - reduction, ply + 1, isPvNode ? -beta : -alpha - 1, -alpha);
+            if (!isPvNode) {
+                if (alpha < score && score < beta && reduction > 1) 
+                    score = -Search(depth - 1, ply + 1, -alpha - 1, -alpha);
                 if (alpha < score && score < beta) 
                     score = -Search(depth - 1, ply + 1, -beta, -score);
             }
+            
             board.UndoMove(move);
 
+            // Update our bests if this move is the best we've seen.
             if (score > result) {
                 result = score;
                 bestMove = move;
-                if (isRoot) {
-                    choice = move;
-#if DEBUG
-                    choiceScore = result;
-                    //Debug(0, "A=" + alpha + " B=" + beta + "; found score = " + score + "; updating current: " + choice.ToString());
-#endif
-                }
 
-                if (score > alpha) {
-                    alpha = score;
-                    if (!move.IsCapture) {
-                        // History heuristic
-                    }
+                // If we're on the first ply (root), save this best move.
+                if (!notRoot) choice = move;
 
-                }
+                // Update alpha with the new score.
+                alpha = Math.Max(alpha, score);
 
+                // Save killer move if applicable.
                 if (score >= beta) {
                     if (!move.IsCapture && killers[ply, 0] != move) {
                         killers[ply, 1] = killers[ply, 0];
                         killers[ply, 0] = move;
                     }
-                    break;
                 }
+
+                // Beta cutoff.
+                if (alpha >= beta) break;
             }
         }
 
-        if (!bestMove.IsNull) {
-            tt[key % 0x7FFFFF] = (
-                bestMove,
-                key,
-                depth,
-                result >= beta ? 2 : result > oAlpha ? 0 : 1,
-                result
-            );
-        }
+        // If we have a best move, save it in the transposition table.
+        tt[key % 0x7FFFFF] = (
+            bestMove,
+            key,
+            depth,
+            // Determine the flag based on the best result we have. 
+            result >= beta ? 2 : result > oAlpha ? 0 : 1,
+            result
+        );
 
         return result;
     }    
 
-    int Score() {
-        PieceList[] lists = board.GetAllPieceLists();
+    /*
+        Evaluation copy/pasta from Tier 2.
+    */
+    int Evaluate() {
         int mg = 0, eg = 0, phase = 0;
 
-        foreach (PieceList list in lists) {
-            foreach (Piece piece in list) {
-                int type_index = (int)piece.PieceType,
-                    turn = piece.IsWhite == board.IsWhiteToMove ? 1 : -1,
-                    i = type_index - 1,
-                    j = Math.Abs(piece.Square.Rank - (piece.IsWhite ? 0 : 7)),
-                    file = (7-piece.Square.File) * 8;
-                
-                ulong mgRank = mg_pesto_values[i, j],
-                      egRank = eg_pesto_values[i, j];
-                
-                mg += (int)((((mgRank >> file) & 255) - 127) * 2 + mg_piece_value[type_index]) * turn;
-                eg += (int)((((egRank >> file) & 255) - 127) * 2 + eg_piece_value[type_index]) * turn;
-                phase += phase_incs[type_index];
+        foreach(bool stm in new[] {true, false}) {
+            for(var p = PieceType.Pawn; p <= PieceType.King; p++) {
+                int piece = (int)p, ind;
+                ulong mask = board.GetPieceBitboard(p, stm);
+                while(mask != 0) {
+                    phase += phase_incs[piece];
+                    ind = 128 * (piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref mask) ^ (stm ? 56 : 0);
+                    mg += getPstVal(ind) + mg_piece_value[piece];
+                    eg += getPstVal(ind + 64) + eg_piece_value[piece];
+                }
             }
+
+            mg = -mg;
+            eg = -eg;
         }
-        phase = Math.Min(phase, 24);
 
-        return (mg * phase + eg * (24-phase)) / 24;
+        return (mg * phase + eg * (24 - phase)) / 24 * (board.IsWhiteToMove ? 1 : -1);
     }
 
-#if DEBUG
-    void Debug(int depth, string text) {
-        System.Console.WriteLine(String.Concat(new string('\t', depth), text));
+    int getPstVal(int psq) {
+        return (int)(((psts[psq / 10] >> (6 * (psq % 10))) & 63) - 20) * 8;
     }
-#endif
 
-
-
+    ulong[] psts = {657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902};
+    int[] mg_piece_value = { 0, 82, 337, 365, 477, 1025, 10000 };
+    int[] eg_piece_value = { 0, 94, 281, 297, 512, 936, 10000 };
+    int[] phase_incs = { 0, 0, 1, 1, 2, 4, 0 };
 }
